a899debdc16f2963d252488ec0c7c85d53b18567
Chathura Yapa <chathura@ssrc.tii.ae>
2024-04-10 10:46:18 +0300

    Black formatted

--- flight_passport/settings.py
+++ flight_passport/settings.py
@@ -94,7 +94,7 @@ MIDDLEWARE = (
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ORIGIN_WHITELIST = [
-     os.environ.get("JWT_ISSUER_DOMAIN", "https://local.test:9000"),
+    os.environ.get("JWT_ISSUER_DOMAIN", "https://local.test:9000"),
 ]
 
 ROOT_URLCONF = "flight_passport.urls"
