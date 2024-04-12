955996cbd013af065662132dea7c17c0d396a3c9
Chathura Yapa <chathura@ssrc.tii.ae>
2024-04-12 09:10:41 +0300

    Added new test cases

--- oauth2_provider_jwt/test_views.py
+++ oauth2_provider_jwt/test_views.py
@@ -14,10 +14,11 @@ class Oauth2ProvideJWTTokenPOSTTests(APITestCase):
     def setUp(self):
         self.api_url = reverse("oauth2_provider_jwt:token")
 
-    def test_token_view_post(self):
+    def test_token_view_post_invalid_grant_type(self):
         data = {
             "grant_type": "your_grant_type",
         }
         response = self.client.post(self.api_url, data, content_type="application/json")
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.json(), {"error": "unsupported_grant_type"})
