c3d5184ce2681b5098d065ee35dac5447b750368
Chathura Yapa <chathura@ssrc.tii.ae>
2024-06-06 10:14:27 +0300

    Append hostname (#4)

--- flight_passport/settings.py
+++ flight_passport/settings.py
@@ -41,7 +41,8 @@ ALLOWED_HOSTS = ["*"]
 issuer_domain = os.environ.get("JWT_ISSUER_DOMAIN", None)
 if issuer_domain:
     d = urlparse(issuer_domain).hostname
-    ALLOWED_HOSTS = [d]
+    if d is not None:
+        ALLOWED_HOSTS.append(d)
     CSRF_TRUSTED_ORIGINS = [issuer_domain]
     CORS_ORIGIN_WHITELIST = [issuer_domain]
 
